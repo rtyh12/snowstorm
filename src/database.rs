7c05b004f88766844f3ac5712d496a3d75218a82
fairydust <rtyh12@gmail.com>
2023-06-22 14:21:06 +0300

    /users works but stupid

--- src/database.rs
+++ src/database.rs
@@ -1,52 +1,67 @@
-pub mod database {
-    use rusqlite::{Connection, Result};
-    use std::fmt;
-    use serde::{Serialize, Deserialize};
+pub mod user;
+use crate::database::user::User;
 
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct Person {
-        id: i32,
-        name: String,
-        data: Option<Vec<u8>>,
+use rusqlite::{Connection, Result};
+
+#[axum::async_trait]
+pub trait Database {
+    async fn get_users(&self) -> Vec<User>;
+}
+
+pub async fn dbtest() -> Result<Vec<User>> {
+    let conn: Connection = Connection::open("test.db")?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS User (
+            id   INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            data BLOB
+        )",
+        (),
+    )?;
+    let me = User {
+        id: 0,
+        name: "Steven".to_string(),
+        data: None,
+    };
+    conn.execute(
+        "INSERT INTO User (name, data) VALUES (?1, ?2)",
+        (&me.name, &me.data),
+    )?;
+
+    let mut stmt = conn.prepare("SELECT id, name, data FROM User")?;
+    let user_iter = stmt.query_map([], |row| {
+        Ok(User {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            data: row.get(2)?,
+        })
+    })?;
+
+    user_iter.collect()
+    // Ok("none".to_string())
+}
+
+pub struct MockDatabase;
+
+#[axum::async_trait]
+impl Database for MockDatabase {
+    async fn get_users(&self) -> Vec<User> {
+        vec![
+            User {
+                id: 0,
+                name: "Hello".to_string(),
+                data: None,
+            },
+            User {
+                id: 1,
+                name: "Goofy".to_string(),
+                data: None,
+            },
+        ]
     }
+}
 
-    impl fmt::Display for Person {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            write!(f, "{}", self.name)
-        }
-    }
-
-    pub fn dbtest() -> Result<Vec<Person>> {
-        let conn = Connection::open("test.db")?;
-
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS person (
-                id   INTEGER PRIMARY KEY,
-                name TEXT NOT NULL,
-                data BLOB
-            )",
-            (), // empty list of parameters.
-        )?;
-        let me = Person {
-            id: 0,
-            name: "Steven".to_string(),
-            data: None,
-        };
-        conn.execute(
-            "INSERT INTO person (name, data) VALUES (?1, ?2)",
-            (&me.name, &me.data),
-        )?;
-
-        let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-        let person_iter = stmt.query_map([], |row| {
-            Ok(Person {
-                id: row.get(0)?,
-                name: row.get(1)?,
-                data: row.get(2)?,
-            })
-        })?;
-        
-        person_iter.collect()
-        // Ok("none".to_string())
-    }
+pub async fn get_users(database: &impl Database) -> Vec<User> {
+    database.get_users().await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,18 @@
-use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    // response::IntoResponse,
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
+use axum::{routing::get, Router};
 use std::net::SocketAddr;
 use Iterator;
-// mod database;
-pub mod database;
-use crate::database::database::dbtest;
+mod database;
+
+use crate::database::{Database, MockDatabase};
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
     tracing_subscriber::fmt::init();
 
-    // build our application with a route
     let app = Router::new()
         .route("/", get(root))
-        .route("/users", post(create_user))
-        .route("/db", get(db));
+        .route("/users", get(users_db));
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([192, 168, 0, 202], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
@@ -32,49 +21,19 @@ async fn main() {
         .unwrap();
 }
 
-async fn db() -> String {
-    let test = match dbtest() {
-        Ok(person) => person,
-        Err(error) => panic!("Problem: {:?}", error),
-    };
-
-    // test.iter()
-    //     .map(|person| person.to_string())
-    //     .collect();
-
-    "efy".to_string()
+async fn users(database_connection: &impl Database) -> String {
+    let test = database::get_users(database_connection).await;
+    serde_json::to_string_pretty(&test).unwrap()
+    // "a".to_string()
 }
 
-// basic handler that responds with a static string
+async fn users_db() -> String {
+    let database_connection = MockDatabase {};
+    let test = database::get_users(&database_connection).await;
+    serde_json::to_string_pretty(&test).unwrap()
+    // "a".to_string()
+}
+
 async fn root() -> &'static str {
     "Hello, Worldyyy!"
-}
-
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
 }
