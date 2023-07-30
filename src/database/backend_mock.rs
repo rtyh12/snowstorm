be1fe39ccaa479e52ba4e13f6763c1fe01790e76
fairydust <rtyh12@gmail.com>
2023-07-31 00:14:24 +0300

    stuff1

--- src/database/backend_mock.rs
+++ src/database/backend_mock.rs
@@ -8,13 +8,15 @@ impl Database for MockDatabase {
         vec![
             database_models::Post {
                 id: 0,
-                name: "Hello".to_string(),
-                data: None,
+                user_id: 0,
+                content: "Hello".to_string(),
+                parent_id: None,
             },
             database_models::Post {
                 id: 1,
-                name: "Goofy".to_string(),
-                data: None,
+                user_id: 1,
+                content: "Woah".to_string(),
+                parent_id: None,
             },
         ]
     }
--- src/database/database_models.rs
+++ src/database/database_models.rs
@@ -2,7 +2,14 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Post {
-    pub id: i32,
+    pub id: i128,
+    pub user_id: i64,
+    pub content: String,
+    pub parent_id: Option<i128>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub id: i64,
     pub name: String,
-    pub data: Option<Vec<u8>>,
 }
